a10c0c19e1251d29d46af912a124657e437936fe
huykt1111 <huylmht10@gmail.com>
2022-04-26 08:02:51 +0700

    Animal

--- DiemDanh26/Animal_NguyenQuangHuy/Animal_NguyenQuangHuy/Program.cs
+++ DiemDanh26/Animal_NguyenQuangHuy/Animal_NguyenQuangHuy/Program.cs
@@ -2,38 +2,49 @@
 
 namespace Animal_NguyenQuangHuy
 {
+    class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine(" Animal is eat. . .");
+        }
+        public void makeSound()
+        {
+            Console.WriteLine(" Animal is makeSound. . .");
+        }
+
+        public virtual void run()
+        {
+
+        }
+        public virtual void fly()
+        {
+
+        }
+    }
+    class Cat : Animal
+    {
+        public override void run()
+        {
+            Console.WriteLine(" Cat is run. . .");
+        }
+    }
+    class Bird : Animal
+    {
+        public override void fly()
+        {
+            Console.WriteLine(" Bird is fly. . .");
+        }
+    }
     class Program
     {
-        class Animal
-        {
-            public  void  eat()
-            {
-                Console.WriteLine(" Animal is eat. . .");
-            }
-            public  void makeSound()
-            {
-                Console.WriteLine(" Animal is makeSound. . .");
-            }
-        }
-        class Cat : Animal
-        {
-            public void run()
-            {
-                Console.WriteLine(" Cat is run. . .");
-            }
-        }
-        class Bird : Animal
-        {
-            public void fly()
-            {
-                Console.WriteLine(" Bird is fly. . .");
-            }
-        }
+        
         static void Main(string[] args)
         {
             Animal cat224 = new Cat();
             Animal bird224 = new Bird();
-            cat.;
+            cat224.run();
+            bird224.fly();
         }
     }
 }
