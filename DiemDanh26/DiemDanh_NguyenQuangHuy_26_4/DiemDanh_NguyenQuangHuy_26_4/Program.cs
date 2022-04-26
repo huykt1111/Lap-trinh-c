7dcdb3483a14ea31dabfc1dc7f20291cc59fe435
huykt1111 <huylmht10@gmail.com>
2022-04-26 07:46:17 +0700

    ptb2

---
+++ DiemDanh26/DiemDanh_NguyenQuangHuy_26_4/DiemDanh_NguyenQuangHuy_26_4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DiemDanh_NguyenQuangHuy_26_4
+{
+    class PTBH224
+    {
+        private double a224, b224, c224;
+
+        public PTBH224()
+        {
+        }
+
+        public PTBH224(double a224, double b224, double c224)
+        {
+            this.a224 = a224;
+            this.b224 = b224;
+            this.c224 = c224;
+        }
+
+        public double A224 { get => a224; set => a224 = value; }
+        public double B224 { get => b224; set => b224 = value; }
+        public double C224 { get => c224; set => c224 = value; }
+
+        public double delta224(double a224, double b224, double c224)
+        {
+            return b224 * b224 - 4 * a224 * c224;
+        }
+
+        public void GiaiHPT(double a224, double b224, double c224)
+        {
+            if (delta224(a224, b224, c224) > 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem : ");
+                Console.WriteLine("X1 = {0}", ((-b224 - Math.Sqrt(delta224(a224,b224,c224)) / 2 * a224)));
+                Console.WriteLine("X2 = {0}", ((-b224 + Math.Sqrt(delta224(a224, b224, c224))) / 2 * a224));
+
+            }
+            else if (delta224(a224, b224, c224) == 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem kep nghiem");
+                Console.WriteLine("X1 = X2 {0}", -b224 / 2 * a224);
+            }
+            else if (delta224(a224, b224, c224) < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a224, b224, c224;
+            Console.WriteLine("Nhap vao a : ");
+            a224 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap vao b : ");
+            b224 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap vao c : ");
+            c224 = float.Parse(Console.ReadLine());
+            PTBH224 ptb2 = new PTBH224();
+            ptb2.GiaiHPT(a224, b224, c224);
+        }
+    }
+}
