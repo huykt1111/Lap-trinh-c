54abce21255668bdb5bf5e2840484ccc5752150d
huykt1111 <huylmht10@gmail.com>
2022-04-26 07:57:17 +0700

    Animal

---
+++ DiemDanh26/Animal_NguyenQuangHuy/Animal_NguyenQuangHuy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Animal_NguyenQuangHuy
+{
+    class Program
+    {
+        class Animal
+        {
+            public  void  eat()
+            {
+                Console.WriteLine(" Animal is eat. . .");
+            }
+            public  void makeSound()
+            {
+                Console.WriteLine(" Animal is makeSound. . .");
+            }
+        }
+        class Cat : Animal
+        {
+            public void run()
+            {
+                Console.WriteLine(" Cat is run. . .");
+            }
+        }
+        class Bird : Animal
+        {
+            public void fly()
+            {
+                Console.WriteLine(" Bird is fly. . .");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Animal cat224 = new Cat();
+            Animal bird224 = new Bird();
+            cat.;
+        }
+    }
+}
