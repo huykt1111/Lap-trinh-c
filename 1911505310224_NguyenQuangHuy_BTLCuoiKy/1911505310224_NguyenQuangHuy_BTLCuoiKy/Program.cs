4de23a3e8cb50db98cb2adf22bdab4fdf32b1cf7
huykt1111 <huylmht10@gmail.com>
2022-06-11 13:35:57 +0700

    Huy

---
+++ 1911505310224_NguyenQuangHuy_BTLCuoiKy/1911505310224_NguyenQuangHuy_BTLCuoiKy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1911505310224_NguyenQuangHuy_BTLCuoiKy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
